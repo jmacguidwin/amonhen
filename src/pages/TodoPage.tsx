9414626c9329efb529dc9cdae6f29526e7f29313
jmacguid <148825427+jmacguid@users.noreply.github.com>
2024-03-11 10:14:51 -0400

    added folder structure for pages and components added initial styling for the camera boxes

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import CameraLayout from "../components/components/layouts/CameraLayout";
 
 type Props = {}
 
 const Todopage = (props: Props) => {
     return (
-        <div>
+        <CameraLayout>
             TodoPage
-        </div>
+        </CameraLayout>
     )   
 }
 
